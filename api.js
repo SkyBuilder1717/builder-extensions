6723e3fa7a051638664d9b14faa39308de35393a
BlackImpostor <106878493+SkyBuilder1717@users.noreply.github.com>
2024-12-20 14:42:23 +0300

    Theme save

--- api.js
+++ api.js
@@ -8,18 +8,19 @@ extensions.forEach(extension => {
     const div = document.createElement('div');
     div.classList.add('extension');
     let text = `<h3>${extension.name}</h3>`;
-        if (extension.link) {
-        text = text + `<p>Author: <a href="${extension.link}">${extension.author}</a></p>`;
-    } else {
-        text = text + `<p>Author: ${extension.author}</p>`;
-    }
     div.innerHTML = text;
+    if (extension.link) {
+        text = `<p>Author: <a href="${extension.link}">${extension.author}</a></p>`;
+    } else {
+        text = `<p>Author: ${extension.author}</p>`;
+    }
     const downloadButton = document.createElement('img');
     downloadButton.src = `/extensions/${extension.id}.png`;
     downloadButton.alt = `Download ${extension.name}`;
     downloadButton.style.cursor = "pointer";
     const downloadUrl = `/extensions/${extension.id}.js`;
     downloadButton.onclick = () => downloadExtension(downloadUrl);
+    div.innerHTML = div.innerHTML + text;
     div.appendChild(downloadButton);
     container.appendChild(div);
 });
@@ -33,8 +34,11 @@ function downloadExtension(url) {
     document.body.removeChild(link);
 }
 
+let darkMode = localStorage.getItem('darkMode') === 'true';
 const toggleButton = document.getElementById('toggle-theme');
 toggleButton.onclick = () => {
+    darkMode = !darkMode;
     document.body.classList.toggle('dark-mode');
     toggleButton.innerHTML = document.body.classList.contains('dark-mode') ? '<img src="/img/moon.png" />' : '<img src="/img/sun.png" />';
+    localStorage.setItem('darkMode', darkMode);
 };
