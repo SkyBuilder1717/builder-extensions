11430fd78b193e3c124eceea45f1d1ae70a96c7d
BlackImpostor <106878493+SkyBuilder1717@users.noreply.github.com>
2024-12-20 13:26:46 +0300

    API update

--- api.js
+++ api.js
@@ -1,18 +1,29 @@
 const extensions = [
-    { name: "Extension 1", description: "This extension does amazing things.", url: "/extensions/extension3.json" }
+    {id: "Geometry_Dash_Tools", name: "Geometry Dash Tools", author: "SkyBuilder1717", link: "https://scratch.mit.edu/users/SkyBuilder1717/"}
 ];
+
 const container = document.getElementById('extensions');
+
 extensions.forEach(extension => {
     const div = document.createElement('div');
     div.classList.add('extension');
-    div.innerHTML = `<h3>${extension.name}</h3><p>${extension.description}</p>`;
-    const downloadButton = document.createElement('button');
-    downloadButton.innerText = "Download";
-    downloadButton.classList.add('download-button');
-    downloadButton.onclick = () => downloadExtension(extension.url);
+    const text = `<h3>${extension.name}</h3>`;
+    if (extension.link) {
+        text = text + `<p>Author: <a href="${extension.link}">${extension.author}</a></p>`;
+    } else {
+        text = text + `<p>Author: ${extension.author}</p>`;
+    }
+    div.innerHTML = text;
+    const downloadButton = document.createElement('img');
+    downloadButton.src = `/extensions/${extension.id}.png`;
+    downloadButton.alt = `Download ${extension.name}`;
+    downloadButton.style.cursor = "pointer";
+    const downloadUrl = `/extensions/${extension.id}.js`;
+    downloadButton.onclick = () => downloadExtension(downloadUrl);
     div.appendChild(downloadButton);
     container.appendChild(div);
 });
+
 function downloadExtension(url) {
     const link = document.createElement('a');
     link.href = url;
